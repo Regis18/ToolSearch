401f2416719b0e5e55b078025cfea84b65d4b270
Regis Humana <regis.humana@fundacion-jala.org>
2019-02-20 10:16:54 -0400

    Reviewed and edited the observations

--- src/main/java/com/jala/utils/Logs.java
+++ src/main/java/com/jala/utils/Logs.java
@@ -1,3 +1,15 @@
+/**
+ * @(#)File.java Copyright (c) 2019 Jala Foundation.
+2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+Jala Foundation, ("Confidential Information"). You shall not
+disclose such Confidential Information and shall use it only in
+accordance with the terms of the license agreement you entered into
+with Jala Foundation.
+ */
+
 package com.jala.utils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -36,6 +48,4 @@ public class Logs {
         }
         return instance;
     }
-
-
 }
