6db6d7e851abe39dbf81e213a6b46ae837d6d395
AreliezVargas <areliez.v25@gmail.com>
2019-03-01 20:45:35 -0400

    Add redimentions in convert method

--- src/main/java/com/jala/convertor/images/models/ConvertorImage.java
+++ src/main/java/com/jala/convertor/images/models/ConvertorImage.java
@@ -9,10 +9,13 @@ import org.im4java.core.IMOperation;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class ConvertorImage implements IConvertible {
 
     private String pathIM = "C:\\WorkingSpaceProg102\\secondSprint\\ToolSearch\\src\\main\\resources\\ThirdParty\\ImageMagick-7.0.8-28-portable-Q16-x64";
+
+    private URL IM = this.getClass().getResource("");
 
     @Override
     public void convert(CriteriaConvertor criteriaConvertor) {
@@ -28,6 +31,8 @@ public class ConvertorImage implements IConvertible {
                 convertImage.addImage(pathAbsoluteNewImage);
                 if (criteriaConvertor.getHorizontal() > 0 && criteriaConvertor.getVertical() > 0) {
                     convertImage.sample(criteriaConvertor.getHorizontal(), criteriaConvertor.getVertical());
+                } else if (criteriaConvertor.getHorizontal() > 0 && criteriaConvertor.getVertical() == -1) {
+
                 }
                 convertCmd.run(convertImage);
             } catch (InterruptedException interruptedException) {
@@ -44,5 +49,9 @@ public class ConvertorImage implements IConvertible {
         if (criteriaConvertor.getNewFileName().isEmpty()) {
             criteriaConvertor.setNewFileName(imageToChange.getName());
         }
+    }
+
+    public void printURL() {
+        System.out.println(IM.toString());
     }
 }
