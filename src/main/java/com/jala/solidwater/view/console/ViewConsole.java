f16ec162ff52162fc0ea474954958aa4c63f503a
raulchoque <yfert_0209@hotmail.com>
2019-03-05 09:23:30 -0400

    update the ViewConsole class added, the methods addRow(List<String> row) and errorPrint(String message) were implement

--- src/main/java/com/jala/solidwater/view/console/ViewConsole.java
+++ src/main/java/com/jala/solidwater/view/console/ViewConsole.java
@@ -14,13 +14,11 @@
 
 package com.jala.solidwater.view.console;
 
-import org.apache.commons.io.FilenameUtils;
-
-import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * ViewConsole class
+ * ViewConsole class, show the data for console
  *
  * @author Raúl Choque
  * @version 0.0.1
@@ -28,27 +26,116 @@ import java.util.List;
 public class ViewConsole {
 
     /**
-     * @param files are all files for be displayed into a table for console.
+     * This list is the size for each column
      */
-    public void testShow (List<File> files){
+    private List<Integer> sizeColums = new ArrayList<>();
 
-        for (int i = 0; i < files.size() ; i++) {
+    /**
+     * This is the counter of each row
+     */
+    private int counterOfRow;
 
-            File file = files.get(i);
-            File path = files.get(i);
-            String name = files.get(i).getName();
-            String extension = FilenameUtils.getExtension(files.get(i).getAbsolutePath());
-
-            System.out.format("%d | %s | %s | %s%n", i, file.getName(), path, extension);
-        }
+    /**
+     * The constructor method of the ViewConsole class to build an instance
+     */
+    public ViewConsole() {
+        sizeColums.add(0);
+        sizeColums.add(0);
+        sizeColums.add(0);
+        sizeColums.add(0);
+        sizeColums.add(0);
+        counterOfRow = 0;
     }
 
     /**
-     * Test method for show a simulate of table.
+     * Section of getter and setter methods
      */
-   /* public void testShow () {
-        for (int i = 0; i < 10 ; i++) {
-            System.out.format("%d | %s | %s | %s%n", i, "path", "name", "extension");
+    public List<Integer> getSizeColums() {
+        return sizeColums;
+    }
+
+    public void setSizeColums(List<Integer> sizeColums) {
+        this.sizeColums = sizeColums;
+    }
+
+    public int getCounterOfRow() {
+        return ++counterOfRow;
+    }
+
+    public void setCounterOfRow(int counterOfRow) {
+        this.counterOfRow = counterOfRow;
+    }
+
+    /**
+     * This method is to add a row
+     * @param row is a new data to add a table
+     */
+    private void addRow(List<String> row) {
+        setSizeColums(updateSize(row));
+        String topLine = horizontalLine(getSizeColums());
+        String lowerLine = topLine;
+        String leftJustifying = "| %-4d |";
+        for (int i = 0; i < row.size(); i++) {
+            leftJustifying += " %-" + row.get(i).length() + "s |";
         }
-    }*/
+        leftJustifying += "%n";
+        String path = row.get(0);
+        String fileName = row.get(1);
+        String extension = row.get(2);
+        String size = row.get(3);
+        System.out.println(topLine);
+        System.out.format(leftJustifying, getCounterOfRow(), path, fileName, extension, size);
+        System.out.println(lowerLine);
+    }
+
+    /**
+     * @param rows is a list with the new row of data
+     * @return a list with the new size of the column
+     */
+    private List<Integer> updateSize(List<String> rows) {
+        List<Integer> updateSizeColumns = new ArrayList<>();
+        updateSizeColumns.add(0, 6);
+        for (int i = 0; i < rows.size(); i++) {
+            int newSizeColumn = (rows.get(i).length()) + 2;
+            int counterRow = i + 1;
+            if (newSizeColumn > sizeColums.get(counterRow)) {
+                updateSizeColumns.add(counterRow, newSizeColumn);
+            } else {
+                updateSizeColumns.add(counterRow, sizeColums.get(counterRow));
+            }
+        }
+        return updateSizeColumns;
+    }
+
+    /**
+     * This method print a message of error
+     * @param message is a value to print.
+     */
+    public void errorPrint(String message) {
+        String error = "Error: " + message + ".";
+        int sizeWithSpace = error.length() + 2;
+        List<Integer> sizeHorizontalLine = new ArrayList<>();
+        sizeHorizontalLine.add(sizeWithSpace);
+        String topLine = horizontalLine(sizeHorizontalLine);
+        String lowerLine = topLine;
+        String leftJustifying = "| %-" + error.length() + "s |%n";
+        System.out.println(topLine);
+        System.out.format(leftJustifying, error);
+        System.out.print(lowerLine);
+    }
+
+    /**
+     * This method create a line with divisions
+     * @param divisionSize is a array of values for divide a line horizontal
+     */
+    private String horizontalLine (List<Integer> divisionSize) {
+        String border = "+";
+        for (Integer division : divisionSize
+                ) {
+            for (int i = 0; i <= division; i++) {
+                border += (i == division) ? "+" : "-";
+            }
+        }
+        return border;
+    }
 }
