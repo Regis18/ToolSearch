6e2aaafc82200a2e5afcacabe2eb164a86969c3c
AreliezVargas <areliez.v25@gmail.com>
2019-03-06 12:51:44 -0400

    Fix copyright

--- src/main/java/com/jala/convertor/models/ConvertorImage.java
+++ src/main/java/com/jala/convertor/models/ConvertorImage.java
@@ -2,7 +2,7 @@
  * @(#) ConvertorImage.java Copyright (c) 2019 Jala Foundation.
  * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
  * All rights reserved.
- * <p>
+ * 
  * This software is the confidential and proprietary information of
  * Jala Foundation, ("Confidential Information").  You shall not
  * disclose such Confidential Information and shall use it only in
