1f37fa323ae1b05455d20bf87a58678237af219b
melvicaballero <melvi.caballero@gmail.com>
2019-03-11 15:19:19 -0400

    new enum ResolutionVideoEnum

---
+++ src/main/java/com/jala/utils/ResolutionVideoEnum.java
@@ -0,0 +1,55 @@
+/*
+ * @(#) TernaryBooleanEnum.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package com.jala.utils;
+
+/**
+ * ResolutionVideoEnum Enum.
+ * @author Melvi Caballero.
+ * @version 0.0.1
+ */
+public enum ResolutionVideoEnum {
+    ntsc,
+    pal,
+    qntsc,
+    qpal,
+    film,
+    ntsc_film,
+    sqcif,
+    qcif,
+    cif,
+    cif_4,
+    cif_16,
+    qqvga,
+    svga,
+    wvga,
+    wxga,
+    wsxga,
+    wvxga,
+    woxga,
+    wqsxga,
+    whuxga,
+    cga,
+    ega,
+    hd480,
+    hd720,
+    hd1080,
+    k_2,
+    k_4,
+    hgvga,
+    hvga,
+    qhd,
+    kdci_2,
+    kdci_4,
+    uhd2160,
+    uhd4320
+}
