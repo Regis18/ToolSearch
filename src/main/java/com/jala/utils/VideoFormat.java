ff9593c10eeab418f1f78b533df2f184e0389672
melvicaballero <melvi.caballero@gmail.com>
2019-03-11 13:48:04 -0400

    New enum VideoFormat

---
+++ src/main/java/com/jala/utils/VideoFormat.java
@@ -0,0 +1,31 @@
+/*
+ * @(#) TernaryBooleanEnum.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package com.jala.utils;
+
+/**
+ * VideoFormat Enum.
+ * @author Melvi Caballero.
+ * @version 0.0.1
+ */
+public enum VideoFormat {
+    wmv,
+    avi,
+    flv,
+    dv,
+    mpeg,
+    Gif,
+    mp4,
+    divx,
+    DVD,
+    xvid
+}
