848c4d3152ed19d0875d581b8f5592916dc9e42c
AreliezVargas <areliez.v25@gmail.com>
2019-03-13 15:35:21 -0400

    Implement the validation for command

--- src/main/java/com/jala/solidwater/view/console/validators/ValidateCommand.java
+++ src/main/java/com/jala/solidwater/view/console/validators/ValidateCommand.java
@@ -10,12 +10,28 @@ import com.jala.solidwater.view.console.models.DefaultCommands;
  * @version 0.0.1
  */
 public class ValidateCommand implements IValidable<Command> {
-    private DefaultCommands defaultCommands;
+    private DefaultCommands defaultCommands = new DefaultCommands();
 
     @Override
-    public boolean validate(Command argument) {
-       return false;
+    public boolean validate(Command command) {
+        boolean isCommandValid = false;
+        if (command.getAcronym().startsWith("-")) {
+
+            defaultCommands.addDefaultCommands();
+            Command defaultComand = new Command();
+            for (int i = 0; i < defaultCommands.getDefaultCommands().size(); i++) {
+                 defaultComand = defaultCommands.getDefaultCommands().get(i);
+                if (command.getAcronym().equals(defaultComand.getAcronym())) {
+                    isCommandValid = true;
+                    i = defaultCommands.getDefaultCommands().size();
+                } else {
+                    isCommandValid = isCommandValid;
+                }
+            }
+        } else {
+            isCommandValid = isCommandValid;
+        }
+
+       return isCommandValid;
     }
-
-
 }
