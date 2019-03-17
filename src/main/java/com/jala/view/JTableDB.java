2de483820c802d3ecaaa2c93a72d7eedae25d9fc
Cxristian <cxristian.lujan05@gmail.com>
2019-03-17 10:00:29 -0400

    create table data base

---
+++ src/main/java/com/jala/view/JTableDB.java
@@ -0,0 +1,59 @@
+/*
+ * @(#) JTableDB.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package com.jala.view;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Module view, ui/JTableResult.
+ * JTableResult class.
+ *
+ * @version 0.0.1.
+ * @autor Melvi Caballero M.
+ */
+public class JTableDB extends JTable{
+    DefaultTableModel defaultTableM = new DefaultTableModel(0, 0);
+
+    String[] header = new String[]{"Id", "Path", "File Name", "Ext.", "Size"};
+
+    /**
+     * Constructor that setting the table DefaultTableModel.
+     */
+    public JTableDB() {
+        super();
+        defaultTableM.setColumnIdentifiers(header);
+
+        //set model into the table object
+        this.setModel(defaultTableM);
+        this.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+        this.getColumnModel().getColumn(0).setPreferredWidth(50);
+        this.getColumnModel().getColumn(1).setPreferredWidth(500);
+        this.getColumnModel().getColumn(2).setPreferredWidth(400);
+        this.getColumnModel().getColumn(3).setPreferredWidth(150);
+        this.getColumnModel().getColumn(4).setPreferredWidth(250);
+    }
+
+    /**
+     * Add a row to the table.
+     *
+     * @param path     file path.
+     * @param fileName file name.
+     * @param ext      file extension.
+     * @param size     file size.
+     */
+    public void addResultRow(String id, String path, String fileName, String ext, String size) {
+        defaultTableM.addRow(new Object[]{id, path, fileName, ext, size});
+    }
+
+}
---
+++ src/main/java/com/jala/view/JpanelShowDB.java
@@ -0,0 +1,112 @@
+/*
+ * @(#) JpanelShowDB.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package com.jala.view;
+
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import java.awt.LayoutManager;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import javax.swing.JScrollPane;
+
+
+public class JpanelShowDB extends JPanel {
+    private JLabel lblCriteria;
+    private JTextField txtCriteria;
+    private JButton btnSearch;
+    private JTableDB tbDataBase;
+    private Border border;
+
+    public JpanelShowDB(LayoutManager layout) {
+        super(layout);
+        init();
+    }
+
+    public JTableDB getTbDataBase() {
+        return tbDataBase;
+    }
+
+    /**
+     * Gets the Path of the location to search.
+     * @return txtPath, the location of file to search.
+     */
+    public JTextField getTxtCriteria() {
+        return txtCriteria;
+    }
+
+    /**
+     * Sets the path field for the search location.
+     * @param txtCriteria, the location where to search.
+     */
+    public void setTxtCriteria(JTextField txtCriteria) {
+        this.txtCriteria = txtCriteria;
+    }
+
+    /**
+     * Gets the search button.
+     * @return btnSearch, the Search button.
+     */
+    public JButton getBtnSearch() {
+        return btnSearch;
+    }
+
+    /**
+     * The init method will initialize this panel.
+     */
+    public void init() {
+        initComponent();
+    }
+
+    private void initComponent() {
+        JPanel pnlNorth = pnlCriteria();
+        this.add(pnlNorth, BorderLayout.NORTH);
+
+        JPanel pnlSouth = pnlTableDB();
+        this.add(pnlSouth, BorderLayout.CENTER);
+    }
+
+    /**
+     * Return the instance of panel central with the path, file name, extension and Search button.
+     * @return pnlCriteria  the panel with path, file name, extension.
+     */
+    private JPanel pnlCriteria() {
+        JPanel pnlPath = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        TitledBorder titleBorder = BorderFactory.createTitledBorder(border, "Search");
+        pnlPath.setBorder(titleBorder);
+        lblCriteria = new JLabel("Criteria: ");
+        pnlPath.add(lblCriteria);
+
+        txtCriteria = new JTextField(50);
+        pnlPath.add(txtCriteria);
+
+        btnSearch = new JButton("Search");
+        pnlPath.add(btnSearch);
+        return pnlPath;
+    }
+
+    private JPanel pnlTableDB() {
+        JPanel pnlSearchGral = new JPanel(new BorderLayout());
+        TitledBorder titleBorder = BorderFactory.createTitledBorder(border, "List Search Data Base");
+        pnlSearchGral.setBorder(titleBorder);
+
+        tbDataBase = new JTableDB();
+        JScrollPane scroll = new JScrollPane(tbDataBase);
+        pnlSearchGral.add(scroll, BorderLayout.CENTER);
+        return pnlSearchGral;
+    }
+}
