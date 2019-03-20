a49f035207884844a7b19a5a044e2eb71b3c1a88
AreliezVargas <areliez.v25@gmail.com>
2019-03-20 12:16:32 -0400

    Add documentation

--- src/main/java/com/jala/solidwater/console/validators/ValidInputParameters.java
+++ src/main/java/com/jala/solidwater/console/validators/ValidInputParameters.java
@@ -1,5 +1,20 @@
+/**
+ * @(#)ValidInputParameters.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
 package com.jala.solidwater.console.validators;
 
+/**
+ * ValidInputParameters class to validate the arguments that are sent from console.
+ */
 public class ValidInputParameters implements IValidable<String[]> {
 
     /**
--- src/main/java/com/jala/solidwater/console/validators/ValidValueOfCommand.java
+++ src/main/java/com/jala/solidwater/console/validators/ValidValueOfCommand.java
@@ -1,12 +1,40 @@
+/**
+ * @(#)ValidValueOfCommand.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
 package com.jala.solidwater.console.validators;
 
+/**
+ * ValidValueOfCommand class to validate the value of command.
+ */
 public class ValidValueOfCommand implements IValidable<String> {
+    /**
+     * Save the error message.
+     */
     private String message = "";
 
+    /**
+     * Return message to print in console.
+     *
+     * @return message as string.
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Set the message to print in console.
+     *
+     * @param message for the error.
+     */
     public void setMessage(String message) {
         this.message = message;
     }
@@ -24,7 +52,7 @@ public class ValidValueOfCommand implements IValidable<String> {
         boolean isValid = false;
         if (valueOfCommand.startsWith("'") && valueOfCommand.endsWith("'")) {
             isValid = true;
-        } else if (valueOfCommand.startsWith("-") || !valueOfCommand.startsWith("'")) {
+        } else if (valueOfCommand.startsWith("-") || !valueOfCommand.startsWith("'") && !valueOfCommand.endsWith("'")) {
             setMessage("The value for the command shouldn't start with -");
             isValid = isValid;
         }
