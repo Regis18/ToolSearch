c2ef26acd5204e84d8bfd6da95082b7ba4c1c60d
AreliezVargas <areliez.v25@gmail.com>
2019-03-22 12:03:32 -0400

    Update tests to remove character

--- src/test/java/com/jala/solidwater/console/ValueCustomTest.java
+++ src/test/java/com/jala/solidwater/console/ValueCustomTest.java
@@ -16,29 +16,129 @@ import com.jala.solidwater.console.models.ValueCustom;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.jala.solidwater.console.models.ValueCustom.*;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Class to unit test of Value custom.
+ */
 public class ValueCustomTest {
 
-    private String valuePath;
-    private String valueFileName;
+    /**
+     * Constant to do the assert.
+     */
+    public static final String VALUE_RESULT = "Hello world";
 
+    /**
+     * Value with character to the start and end string.
+     */
+    private String valueWithCharacterToTheStarAndEndInString;
+
+    /**
+     * Value with character to the start and end string and between words.
+     */
+    private String valueWithCharacterBetweenWords;
+
+    /**
+     * Initialize the values before start the test.
+     */
     @Before
-    public void setup(){
+    public void setup() {
 
-        valuePath = "'C:\\hola'";
-        valueFileName = "'hola mundo'";
+        valueWithCharacterToTheStarAndEndInString = "'Hello world'";
+        valueWithCharacterBetweenWords = "'Hello 'wor'ld'";
     }
 
+    /**
+     * Verify that the characters will be removed to the start and end of a string.
+     */
     @Test
-    public void removeTheCharacterOfValueWithSimpleQuotes() {
+    public void removeTheCharactersToTheStartAndEndString() {
         ValueCustom valueCustomTest = new ValueCustom();
-        assertEquals("C:\\hola",valueCustomTest.removeCharSpecial(valuePath, "'", 0));
+        assertEquals(VALUE_RESULT, valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 0));
     }
 
+    /**
+     * Verify that the characters will be removed with spaces between words.
+     */
     @Test
-    public void removeTheCharacterOfValueWithSimpleQuotesAndSpaceBetweenWords() {
+    public void removeTheCharactersWithSpaceBetweenWords() {
         ValueCustom valueCustomTest = new ValueCustom();
-        assertEquals("hola mundo",valueCustomTest.removeCharSpecial(valueFileName, "'", 0));
+        assertEquals(VALUE_RESULT, valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 0));
+        assertEquals(VALUE_RESULT, valueCustomTest.removeCharSpecial(valueWithCharacterBetweenWords, "'", 1));
+    }
+
+    /**
+     * Verify that all coincidence will be removed.
+     */
+    @Test
+    public void removeAllCoincidenceOfCharacter() {
+        ValueCustom valueCustomTest = new ValueCustom();
+        assertEquals(VALUE_RESULT, valueCustomTest.removeCharSpecial(valueWithCharacterBetweenWords, "'", 1));
+    }
+
+    /**
+     * Verify that characters will be removed in a string with length equals to 2.
+     */
+    @Test
+    public void removeCharacterOfValueWithLengthEqualsTo2() {
+        valueWithCharacterToTheStarAndEndInString = "''";
+        ValueCustom valueCustomTest = new ValueCustom();
+        assertEquals("", valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 1));
+        assertEquals("", valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 0));
+    }
+
+    /**
+     * Verify that characters will be removed in a string with length equals to 1.
+     */
+    @Test
+    public void removeTheCharacterOfValueWithLengthEqualsTo1() {
+        valueWithCharacterToTheStarAndEndInString = "'";
+        ValueCustom valueCustomTest = new ValueCustom();
+        assertEquals(ERROR_TO_LENGTH_LESS_THAN_2, valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 0));
+        assertEquals("", valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 1));
+    }
+
+    /**
+     * Verify an error message for string with length equals to 0.
+     */
+    @Test
+    public void removeTheCharacterOfValueWithLengthEqualsTo0() {
+        valueWithCharacterToTheStarAndEndInString = "";
+        ValueCustom valueCustomTest = new ValueCustom();
+        assertEquals(ERROR_TO_LENGTH_LESS_THAN_2, valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 0));
+        assertEquals(ERROR_FOR_LENGTH_LESS_THAN_1, valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 1));
+    }
+
+    /**
+     * Verify that characters will be removed in a string that contains only spaces.
+     */
+    @Test
+    public void removeTheCharacterOfValueOnlyWithSpace() {
+        valueWithCharacterToTheStarAndEndInString = "'  '";
+        ValueCustom valueCustomTest = new ValueCustom();
+        assertEquals("  ", valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 0));
+        assertEquals("  ", valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 1));
+    }
+
+    /**
+     * Verify that characters will be removed in a string that contains spaces in the end.
+     */
+    @Test
+    public void removeTheCharacterOfValueWithSpaceInTheEndOfWord() {
+        valueWithCharacterToTheStarAndEndInString = "'Hello' '";
+        ValueCustom valueCustomTest = new ValueCustom();
+        assertEquals("Hello' ", valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 0));
+        assertEquals("Hello ", valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 1));
+    }
+
+    /**
+     * Verify that the error message when enter a condition no valid.
+     */
+    @Test
+    public void removeTheCharacterSendingInvalidValueToRemove() {
+        ValueCustom valueCustomTest = new ValueCustom();
+        assertEquals(ERROR_FOR_CONDITIONS, valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", -1));
+        assertEquals(ERROR_FOR_CONDITIONS, valueCustomTest.removeCharSpecial(valueWithCharacterToTheStarAndEndInString, "'", 3));
     }
 }
