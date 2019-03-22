481aeeafeaf1f09f65460e48119d329d1d0223fc
Cxristian <cxristian.lujan05@gmail.com>
2019-03-22 03:05:40 -0400

    change of style to tables and edges

---
+++ src/main/java/com/jala/view/CustomTitleBorder.java
@@ -0,0 +1,36 @@
+/*
+ * @(#) CustomTitleBorder.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package com.jala.view;
+
+import javax.swing.BorderFactory;
+
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.plaf.ColorUIResource;
+import java.awt.Color;
+import java.awt.Font;
+
+public class CustomTitleBorder extends TitledBorder {
+
+    public CustomTitleBorder(String title) {
+
+        super(title);
+        setBorder(BorderFactory.createTitledBorder(title));
+        setTitlePosition(TitledBorder.DEFAULT_POSITION);
+        setTitleFont(new Font("Bahnschrift", 1, 12));
+        setTitleColor(new ColorUIResource(43, 77, 198));
+        setBorder(new CompoundBorder(new EmptyBorder(8,8,8,8), new LineBorder(new Color(249, 255, 250), 1)));
+    }
+}
--- src/main/java/com/jala/view/JPanelConverterVideo.java
+++ src/main/java/com/jala/view/JPanelConverterVideo.java
@@ -12,11 +12,7 @@
 
 package com.jala.view;
 
-import java.awt.BorderLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Component;
-import java.awt.Insets;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
