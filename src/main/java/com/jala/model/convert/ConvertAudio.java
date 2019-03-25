3f0ba98e5fd022b0173c3b6af4a440eeacd8b8ad
melvicaballero <melvi.caballero@gmail.com>
2019-03-24 20:11:33 -0400

    fix obs.

--- src/main/java/com/jala/model/convert/ConvertAudio.java
+++ src/main/java/com/jala/model/convert/ConvertAudio.java
@@ -32,108 +32,119 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * This class used the FFmpeg app for Convert of format an audio .
+ *
  * @author Regis Humana
  * @version 0.0.3
  */
 public class ConvertAudio implements IConvertible {
 
-	/**Save the real path destination to send to FFMPEG.*/
-	private String pathDestination;
+    /**
+     * Save the real path destination to send to FFMPEG.
+     */
+    private String pathDestination;
 
-	/** It creates to follow up the instruction of the class.*/
-	private Logger log = Logs.getInstance().getLog();
+    /**
+     * It creates to follow up the instruction of the class.
+     */
+    private Logger log = Logs.getInstance().getLog();
 
-	/** Var for error messages.*/
-	private String convertResult;
+    /**
+     * Var for error messages.
+     */
+    private String convertResult;
 
-	/**
-	 * Convert formats of music into another extension - basic.
-	 * @param criteriaObject criteria for convert audio.
-	 */
-	@Override
-	public void convert(Object criteriaObject) {
-		CriteriaConverterAudio criteria = (CriteriaConverterAudio) criteriaObject;
-		try {
-			String pathFFmpeg = getClass().getClassLoader().getResource("ThirdParty/ffmpeg/bin/").getPath() + "ffmpeg.exe";
-			FFmpeg fmpeg = new FFmpeg(Common.cleanPath(pathFFmpeg));
-			String pathFFprobe = getClass().getClassLoader().getResource("ThirdParty/ffmpeg/bin/").getPath() + "ffprobe.exe";
-			FFprobe fprobe = new FFprobe(Common.cleanPath(pathFFprobe));
-			FFmpegBuilder builder = new FFmpegBuilder();
-			pathDestination = criteria.getPathDestination() + criteria.getNewFileName() + criteria.getNewExtension();
-			builder.addInput(criteria.getPath()).overrideOutputFiles(true);
-			if (! criteria.isAdvanced()) {
-				builder.addOutput(pathDestination);
-				FFmpegProbeResult probeResult = fprobe.probe(criteria.getPath());
-				FFmpegExecutor executor = new FFmpegExecutor(fmpeg, fprobe);
-				ConvertVideoProgress(executor, builder, probeResult);
-			} else {
-				convertAdvancedAudio(criteria, builder, fmpeg, fprobe);
-			}
-		} catch (IOException error) {
-			log.error("Error in ConvertAudio", error);
-		}
-	}
+    /**
+     * Convert formats of music into another extension - basic.
+     *
+     * @param criteriaObject criteria for convert audio.
+     */
+    @Override
+    public void convert(Object criteriaObject) {
+        CriteriaConverterAudio criteria = (CriteriaConverterAudio) criteriaObject;
+        try {
+            String pathFFmpeg = getClass().getClassLoader().getResource("ThirdParty/ffmpeg/bin/ffmpeg.exe").getPath();
+            FFmpeg fmpeg = new FFmpeg(Common.cleanPath(pathFFmpeg));
+            String pathFFprobe = getClass().getClassLoader().getResource("ThirdParty/ffmpeg/bin/ffprobe.exe").getPath();
+            FFprobe fprobe = new FFprobe(Common.cleanPath(pathFFprobe));
+            FFmpegBuilder builder = new FFmpegBuilder();
+            pathDestination = criteria.getPathDestination() + criteria.getNewFileName() + criteria.getNewExtension();
+            builder.addInput(criteria.getPath()).overrideOutputFiles(true);
+            if (!criteria.isAdvanced()) {
+                builder.addOutput(pathDestination);
+                FFmpegProbeResult probeResult = fprobe.probe(criteria.getPath());
+                FFmpegExecutor executor = new FFmpegExecutor(fmpeg, fprobe);
+                ConvertVideoProgress(executor, builder, probeResult);
+            } else {
+                convertAdvancedAudio(criteria, builder, fmpeg, fprobe);
+            }
+        } catch (IOException error) {
+            log.error("Error in ConvertAudio", error);
+        }
+    }
 
-	/**
-	 * Convert the music with advances parameters like audioChannel, BitRate and SampleRate.
-	 * @param criteria for convert audio.
-	 * @param builder FFmpeg builder.
-	 * @param fmpeg converter app.
-	 */
-	private void convertAdvancedAudio(CriteriaConverterAudio criteria, FFmpegBuilder builder, FFmpeg fmpeg, FFprobe fprobe) {
-		try {
-			FFmpegOutputBuilder outputBuilder = new FFmpegOutputBuilder();
-			outputBuilder.setFilename(pathDestination);
-			if (! criteria.getAudioChannel().isEmpty()) {
-				int channel = criteria.getAudioChannel() == "Mono" ? FFmpeg.AUDIO_MONO : FFmpeg.AUDIO_STEREO;
-				outputBuilder.setAudioChannels(channel);
-			}
-			if (criteria.getBitRate() != 0) {
-				outputBuilder.setAudioBitRate(criteria.getBitRate());
-			}
-			if (criteria.getSampleRate() != 0) {
-				outputBuilder.setAudioSampleRate(criteria.getSampleRate());
-			}
-			builder.addOutput(outputBuilder);
-			FFmpegProbeResult probeResult = fprobe.probe(criteria.getPath());
-			FFmpegExecutor executor = new FFmpegExecutor(fmpeg, fprobe);
-			ConvertVideoProgress(executor, builder, probeResult);
-		} catch (IOException error) {
-			log.error("Error in ConvertAdvancedAudio", error);
-		}
-	}
+    /**
+     * Convert the music with advances parameters like audioChannel, BitRate and SampleRate.
+     *
+     * @param criteria for convert audio.
+     * @param builder  FFmpeg builder.
+     * @param fmpeg    converter app.
+     */
+    private void convertAdvancedAudio(CriteriaConverterAudio criteria, FFmpegBuilder builder, FFmpeg fmpeg, FFprobe fprobe) {
+        try {
+            FFmpegOutputBuilder outputBuilder = new FFmpegOutputBuilder();
+            outputBuilder.setFilename(pathDestination);
+            if (!criteria.getAudioChannel().isEmpty()) {
+                int channel = criteria.getAudioChannel() == "Mono" ? FFmpeg.AUDIO_MONO : FFmpeg.AUDIO_STEREO;
+                outputBuilder.setAudioChannels(channel);
+            }
+            if (criteria.getBitRate() != 0) {
+                outputBuilder.setAudioBitRate(criteria.getBitRate());
+            }
+            if (criteria.getSampleRate() != 0) {
+                outputBuilder.setAudioSampleRate(criteria.getSampleRate());
+            }
+            builder.addOutput(outputBuilder);
+            FFmpegProbeResult probeResult = fprobe.probe(criteria.getPath());
+            FFmpegExecutor executor = new FFmpegExecutor(fmpeg, fprobe);
+            ConvertVideoProgress(executor, builder, probeResult);
+        } catch (IOException error) {
+            log.error("Error in ConvertAdvancedAudio", error);
+        }
+    }
 
-	/**
-	 * Method for convert the video and audio using the FFmpeg and FFprobe and show the progress.
-	 * @param executor this method is who do the progress.
-	 * @param builder is who do the convert.
-	 * @param pr is the library.
-	 */
-	public void ConvertVideoProgress(FFmpegExecutor executor, FFmpegBuilder builder, final FFmpegProbeResult pr) {
-		FFmpegJob job = executor.createJob(builder, new ProgressListener() {
-			private final double durationNs = pr.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
-			@Override
-			public void progress(final Progress progress) {
-				double percentage = progress.out_time_ns / durationNs;
-				if((int) (percentage * 100) <= 100) {
-					JPanelConverterVideo.setProgressBarValue((int) (percentage * 100));
-				}
-			}
-		});
-		try {
-			job.run();
-		} catch (Exception e) {
-			e.printStackTrace();
-			log.error(e);
-		}
-		if (job.getState() == FFmpegJob.State.FAILED) {
-			convertResult = "The Convertion has failed. ";
-			log.error("The Convertion has failed.");
-		}
-		if (job.getState() == FFmpegJob.State.FINISHED) {
-			JPanelConverterVideo.setProgressBarValue(100);
-			convertResult = "The Convertion has finished successfully.";
-			log.info("The Convertion has finished successfully.");
-		}
-	}
+    /**
+     * Method for convert the video and audio using the FFmpeg and FFprobe and show the progress.
+     *
+     * @param executor this method is who do the progress.
+     * @param builder  is who do the convert.
+     * @param probeResult       is the library.
+     */
+    public void ConvertVideoProgress(FFmpegExecutor executor, FFmpegBuilder builder, final FFmpegProbeResult probeResult) {
+        FFmpegJob job = executor.createJob(builder, new ProgressListener() {
+            private final double durationNs = probeResult.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
+
+            @Override
+            public void progress(final Progress progress) {
+                double percentage = progress.out_time_ns / durationNs;
+                if ((int) (percentage * 100) <= 100) {
+                    JPanelConverterVideo.setProgressBarValue((int) (percentage * 100));
+                }
+            }
+        });
+        try {
+            job.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error(e);
+        }
+        if (job.getState() == FFmpegJob.State.FAILED) {
+            convertResult = "The Convertion has failed. ";
+            log.error("The Convertion has failed.");
+        }
+        if (job.getState() == FFmpegJob.State.FINISHED) {
+            JPanelConverterVideo.setProgressBarValue(100);
+            convertResult = "The Convertion has finished successfully.";
+            log.info("The Convertion has finished successfully.");
+        }
+    }
 }
--- src/main/java/com/jala/model/convert/ConvertVideo.java
+++ src/main/java/com/jala/model/convert/ConvertVideo.java
@@ -34,114 +34,125 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * This class used the FFmpeg app for Convert of format a Video.
- * @version 0.0.3
+ *
  * @author Regis Humana
+ * @version 0.0.3
  */
 public class ConvertVideo implements IConvertible {
 
-	/** It creates to follow up the instruction of the class.*/
-	private Logger log = Logs.getInstance().getLog();
+    /**
+     * It creates to follow up the instruction of the class.
+     */
+    private Logger log = Logs.getInstance().getLog();
 
-	/** Store the path destination with the path of folder, name of file and the extension.*/
-	private String pathDestination;
+    /**
+     * Store the path destination with the path of folder, name of file and the extension.
+     */
+    private String pathDestination;
 
-	/** Var for error messages.*/
-	private String convertResult;
+    /**
+     * Var for error messages.
+     */
+    private String convertResult;
 
-	/**
-	 * Convert formats of video into another extension - basic.
-	 * @param criteriaObject criteria for convert video.
-	 */
-	public void convert(Object criteriaObject) {
-		CriteriaConverterVideo criteria = (CriteriaConverterVideo) criteriaObject;
-		try {
-			String pathFFmpeg = getClass().getClassLoader().getResource("ThirdParty/ffmpeg/bin/").getPath() + "ffmpeg.exe";
-			FFmpeg fmpeg = new FFmpeg(Common.cleanPath(pathFFmpeg));
-			String pathFFprobe = getClass().getClassLoader().getResource("ThirdParty/ffmpeg/bin/").getPath() + "ffprobe.exe";
-			FFprobe fprobe = new FFprobe(Common.cleanPath(pathFFprobe));
-			FFmpegBuilder builder = new FFmpegBuilder();
-			pathDestination = criteria.getPathDestination() + criteria.getNewFileName() + criteria.getNewExtension();
-			builder.addInput(criteria.getPath()).overrideOutputFiles(true);
-			if (! criteria.getIsAdvanced()) {
-				builder.addOutput(pathDestination);
-				FFmpegProbeResult probeResult = fprobe.probe(criteria.getPath());
-				FFmpegExecutor executor = new FFmpegExecutor(fmpeg, fprobe);
-				ConvertVideoProgress(executor, builder, probeResult);
-			} else {
-			convertAdvancedVideo(criteria, builder, fmpeg, fprobe);
-			}
-		} catch (IOException error) {
-			log.error("Error in ConvertVideo", error);
-		}
-	}
+    /**
+     * Convert formats of video into another extension - basic.
+     *
+     * @param criteriaObject criteria for convert video.
+     */
+    public void convert(Object criteriaObject) {
+        CriteriaConverterVideo criteria = (CriteriaConverterVideo) criteriaObject;
+        try {
+            String pathFFmpeg = getClass().getClassLoader().getResource("ThirdParty/ffmpeg/bin/ffmpeg.exe").getPath();
+            FFmpeg fmpeg = new FFmpeg(Common.cleanPath(pathFFmpeg));
+            String pathFFprobe = getClass().getClassLoader().getResource("ThirdParty/ffmpeg/bin/ffprobe.exe").getPath();
+            FFprobe fprobe = new FFprobe(Common.cleanPath(pathFFprobe));
+            FFmpegBuilder builder = new FFmpegBuilder();
+            pathDestination = criteria.getPathDestination() + criteria.getNewFileName() + criteria.getNewExtension();
+            builder.addInput(criteria.getPath()).overrideOutputFiles(true);
+            if (!criteria.getIsAdvanced()) {
+                builder.addOutput(pathDestination);
+                FFmpegProbeResult probeResult = fprobe.probe(criteria.getPath());
+                FFmpegExecutor executor = new FFmpegExecutor(fmpeg, fprobe);
+                ConvertVideoProgress(executor, builder, probeResult);
+            } else {
+                convertAdvancedVideo(criteria, builder, fmpeg, fprobe);
+            }
+        } catch (IOException error) {
+            log.error("Error in ConvertVideo", error);
+        }
+    }
 
-	/**
-	 * Convert the video with advances parameters like audioChannel, BitRate, SampleRate, Frame rate & Video resolution.
-	 * @param criteria for convert video and audio.
-	 * @param builder for the format convert audio and video.
-	 * @param fmpeg library.
-	 * @param fprobe library.
-	 */
-	private void convertAdvancedVideo(CriteriaConverterVideo criteria, FFmpegBuilder builder, FFmpeg fmpeg, FFprobe fprobe) {
-			try {
-			FFmpegOutputBuilder outputBuilder = new FFmpegOutputBuilder();
-			outputBuilder.setFilename(pathDestination);
-			if (! criteria.getAudioChannel().isEmpty()) {
-				int channel = criteria.getAudioChannel() == "Mono" ? FFmpeg.AUDIO_MONO : FFmpeg.AUDIO_STEREO;
-				outputBuilder.setAudioChannels(channel);
-			}
-			if (criteria.getFrameRate() != 0) {
-				outputBuilder.setVideoFrameRate(criteria.getFrameRate());
-			}
-			if (! criteria.getVideoResolution().isEmpty()) {
-				outputBuilder.setVideoResolution(criteria.getVideoResolution());
-			}
-			if (criteria.getBitRate() != 0) {
-				outputBuilder.setAudioBitRate(criteria.getBitRate());
-			}
-			if (criteria.getSampleRate() != 0) {
-				outputBuilder.setAudioSampleRate(criteria.getSampleRate());
-			}
-			builder.addOutput(outputBuilder);
-			FFmpegExecutor executor = new FFmpegExecutor(fmpeg, fprobe);
-			FFmpegProbeResult probeResult = fprobe.probe(criteria.getPath());
-			ConvertVideoProgress(executor, builder, probeResult);
-		} catch (IOException error) {
-			log.error("Error in ConvertAdvancedAudio", error);
-		}
-	}
+    /**
+     * Convert the video with advances parameters like audioChannel, BitRate, SampleRate, Frame rate & Video resolution.
+     *
+     * @param criteria for convert video and audio.
+     * @param builder  for the format convert audio and video.
+     * @param fmpeg    library.
+     * @param fprobe   library.
+     */
+    private void convertAdvancedVideo(CriteriaConverterVideo criteria, FFmpegBuilder builder, FFmpeg fmpeg, FFprobe fprobe) {
+        try {
+            FFmpegOutputBuilder outputBuilder = new FFmpegOutputBuilder();
+            outputBuilder.setFilename(pathDestination);
+            if (!criteria.getAudioChannel().isEmpty()) {
+                int channel = criteria.getAudioChannel() == "Mono" ? FFmpeg.AUDIO_MONO : FFmpeg.AUDIO_STEREO;
+                outputBuilder.setAudioChannels(channel);
+            }
+            if (criteria.getFrameRate() != 0) {
+                outputBuilder.setVideoFrameRate(criteria.getFrameRate());
+            }
+            if (!criteria.getVideoResolution().isEmpty()) {
+                outputBuilder.setVideoResolution(criteria.getVideoResolution());
+            }
+            if (criteria.getBitRate() != 0) {
+                outputBuilder.setAudioBitRate(criteria.getBitRate());
+            }
+            if (criteria.getSampleRate() != 0) {
+                outputBuilder.setAudioSampleRate(criteria.getSampleRate());
+            }
+            builder.addOutput(outputBuilder);
+            FFmpegExecutor executor = new FFmpegExecutor(fmpeg, fprobe);
+            FFmpegProbeResult probeResult = fprobe.probe(criteria.getPath());
+            ConvertVideoProgress(executor, builder, probeResult);
+        } catch (IOException error) {
+            log.error("Error in ConvertAdvancedAudio", error);
+        }
+    }
 
-	/**
-	 * Method for convert the video and audio using the FFmpeg and FFprobe and show the progress.
-	 * @param executor this method is who do the progress.
-	 * @param builder is who do the convert.
-	 * @param pr is the library.
-	 */
-	public void ConvertVideoProgress(FFmpegExecutor executor, FFmpegBuilder builder, final FFmpegProbeResult pr) {
-		FFmpegJob job = executor.createJob(builder, new ProgressListener() {
-			private final double durationNs = pr.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
-			@Override
-			public void progress(final Progress progress) {
-				double percentage = progress.out_time_ns / durationNs;
-				if((int) (percentage * 100) <= 100) {
-					JPanelConverterVideo.setProgressBarValue((int) (percentage * 100));
-				}
-			}
-		});
-		try {
-			job.run();
-		} catch (Exception e) {
-			e.printStackTrace();
-			log.error(e);
-		}
-		if (job.getState() == FFmpegJob.State.FAILED) {
-			convertResult = "The Conversion has failed. ";
-			log.error("The Convertion has failed.");
-		}
-		if (job.getState() == FFmpegJob.State.FINISHED) {
-			JPanelConverterVideo.setProgressBarValue(100);
-			convertResult = "The Conversion has finished successfully.";
-			log.info("The Conversion has finished successfully.");
-		}
-	}
+    /**
+     * Method for convert the video and audio using the FFmpeg and FFprobe and show the progress.
+     *
+     * @param executor this method is who do the progress.
+     * @param builder  is who do the convert.
+     * @param probeResult       is the library.
+     */
+    public void ConvertVideoProgress(FFmpegExecutor executor, FFmpegBuilder builder, final FFmpegProbeResult probeResult) {
+        FFmpegJob job = executor.createJob(builder, new ProgressListener() {
+            private final double durationNs = probeResult.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
+
+            @Override
+            public void progress(final Progress progress) {
+                double percentage = progress.out_time_ns / durationNs;
+                if ((int) (percentage * 100) <= 100) {
+                    JPanelConverterVideo.setProgressBarValue((int) (percentage * 100));
+                }
+            }
+        });
+        try {
+            job.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error(e);
+        }
+        if (job.getState() == FFmpegJob.State.FAILED) {
+            convertResult = "The Conversion has failed. ";
+            log.error("The Convertion has failed.");
+        }
+        if (job.getState() == FFmpegJob.State.FINISHED) {
+            JPanelConverterVideo.setProgressBarValue(100);
+            convertResult = "The Conversion has finished successfully.";
+            log.info("The Conversion has finished successfully.");
+        }
+    }
 }
