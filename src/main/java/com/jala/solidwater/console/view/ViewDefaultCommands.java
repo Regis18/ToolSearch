acd45d3dad694ea9b3468d1077b6425b17dce018
AreliezVargas <areliez.v25@gmail.com>
2019-03-26 12:56:05 -0400

    Print default commands

--- src/main/java/com/jala/solidwater/console/view/ViewDefaultCommands.java
+++ src/main/java/com/jala/solidwater/console/view/ViewDefaultCommands.java
@@ -8,9 +8,12 @@ public class ViewDefaultCommands {
 
     public void printCommands(List<Command> defaultCommands) {
 
+        System.out.println("These are common commands used to do the search:");
+        System.out.printf("%-10s %-30s %-20s\n" , "Acronym", "Command name", "Description");
         for (int i = 0; i < defaultCommands.size(); i++) {
             Command defaultCommand = defaultCommands.get(i);
-            System.out.println(defaultCommand.getAcronym() + " " + defaultCommand.getName() + " " + defaultCommand.getDescription());
+           // System.out.format("%-10s %-10s %-10s\\n", defaultCommand.getAcronym()  + defaultCommand.getName() + defaultCommand.getDescription());
+            System.out.printf("%-10s %-30s %-20s\n" , defaultCommand.getAcronym(), defaultCommand.getName(), defaultCommand.getDescription());
         }
     }
 }
