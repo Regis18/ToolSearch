18b6623b1d31d11da522f2ea9a35b216d371803f
AreliezVargas <areliez.v25@gmail.com>
2019-03-28 12:41:54 -0400

    Fix headers name in tables

--- src/main/java/com/jala/view/JPanelAdvanced.java
+++ src/main/java/com/jala/view/JPanelAdvanced.java
@@ -419,7 +419,7 @@ public class JPanelAdvanced extends JPanel implements ActionListener {
         addComponent(lblReadOnly, 3, 2, 1, 1);
 
         lblDateCreateStar = new CustomLabel();
-        lblDateCreateStar.setText("Creation Date From: ");
+        lblDateCreateStar.setText("Creation Date:    From: ");
         addComponent(lblDateCreateStar, 0, 3, 1, 1);
 
         lblDateCreateEnd = new CustomLabel();
